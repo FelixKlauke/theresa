fb1f9e42ccf7e7ea37b9119467ba63d09c6994fb
Felix Klauke <felix@klauke-enterprises.com>
2019-06-10 09:08:41 +0200

    Add warning about empty modules in factory.

--- core/src/main/java/de/d3adspace/theresa/core/TheresaFactory.java
+++ core/src/main/java/de/d3adspace/theresa/core/TheresaFactory.java
@@ -1,6 +1,8 @@
 package de.d3adspace.theresa.core;
 
 import com.google.inject.Module;
+import com.sun.org.slf4j.internal.Logger;
+import com.sun.org.slf4j.internal.LoggerFactory;
 import de.d3adspace.theresa.lifecycle.LifeCycleManager;
 import de.d3adspace.theresa.lifecycle.LifeCycleManagerImpl;
 
@@ -10,6 +12,8 @@ import de.d3adspace.theresa.lifecycle.LifeCycleManagerImpl;
  * @author Felix Klauke <info@felix-klauke.de>
  */
 public class TheresaFactory {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TheresaFactory.class);
 
     TheresaFactory() {
 
@@ -24,6 +28,10 @@ public class TheresaFactory {
      * @return The theresa instance.
      */
     public static Theresa create(Module... modules) {
+
+        if (modules.length == 0) {
+            LOGGER.warn("You're using zero effective modules. Are you sure about that?");
+        }
 
         LifeCycleManager lifeCycleManager = new LifeCycleManagerImpl();
         return new TheresaImpl(lifeCycleManager, modules);
